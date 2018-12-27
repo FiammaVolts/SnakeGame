0fd07b2b37da66d6daa971c84937db6455b213a6
FiammaV <ines.perdigao.nunes@gmail.com>
2018-12-27 15:39:47 +0000

    Added the writelines

--- SnakeGame/Draw.cs
+++ SnakeGame/Draw.cs
@@ -8,5 +8,50 @@ namespace SnakeGame
 {
     class Draw
     {
+
+        public void ShowMenu() {
+            Console.WriteLine("╔═══*.·:·.**  < Snake >  **.·:·.*═══╗");
+            Console.WriteLine();
+            Console.WriteLine("       1. Start Game\n" +
+                              "       2. Highscore\n" +
+                              "       3. Credits\n" + 
+                              "       4. Quit Game\n)";
+            Console.WriteLine("╚═══*.·:·.**  < Game >  **.·:·.*═══╝");
+
+            // Will call the ReadEntry() method
+            ReadEntry();
+        }
+
+        public void ShowWorld() {
+            for (int i = 0; i < World.height; i++) {
+                for (int j = 0; j < World.width; j++) {
+                    if (i == 0 || i == World.height - 1) {
+                        Console.Write("|");
+                    }
+                    else if (j == 0 || j == World.width - 1) {
+                        Console.Write("_ ");
+                    }
+                    else if (j == world.appleX && i == world.appleY) {
+                        Console.Write(apple);
+                    }
+                    else if (j == world.noseX && i == world.noseY) {
+                        Console.Write(snake);
+                    }
+                    else {
+                        world.isPrinted = false;
+                        for (int k = 0; k < world.nTail; k++) {
+                            if (world.TailX[k] == j && world.TailY[k] == i) {
+                                Console.Write(snake);
+                                world.isPrinted = true;
+                            }
+                        }
+                        if (!world.isPrinted)
+                            Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
