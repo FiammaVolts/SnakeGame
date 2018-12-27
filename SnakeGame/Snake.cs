5c422d940ee153eb51929fc3f3c615ba4ce08de7
ineesgoncalvees <ines98goncalves@gmail.com>
2018-12-27 17:18:11 +0000

    Created class that has all the players info

---
+++ SnakeGame/Snake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    class Snake
+    {
+        private int[] TailX = new int[100];
+        private int[] TailY = new int[100];
+
+        private bool horizontal;
+        private bool vertical;
+    }
+}
