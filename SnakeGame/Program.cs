69461e5a1aa4fda17557d8864edb6fe5c6189c84
ineesgoncalvees <ines98goncalves@gmail.com>
2018-12-27 17:16:24 +0000

    Called Update()

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -8,38 +8,14 @@ namespace SnakeGame
 {
     class Program
     {
-
-        public World world;
-        public GameManager gm;
-
-
-        public void Start()
-        {
-
-            world.dir = "RIGHT";
-            world.pre_dir = "";
-            world.points = 0;
-            world.nTail = 0;
-
-            gm.gameOver = false;
-            gm.reset = false;
-            world.isPrinted = false;
-
-            world.noseX = World.width / 2;
-            world.noseY = World.height / 2;
-            world.appleX = world.rnd.Next(1, World.width - 1);
-            world.appleY = world.rnd.Next(1, World.height - 1);
-        }
-
         static void Main(string[] args)
         {
+            GameManager gm = new GameManager();
 
-            Program prog = new Program();
-
-            while (true)
-            {
-                prog.Start();
-            }
+            //gm.Start();
+            gm.Update();
+            
+            
         }
     }
 }
