250bfb7fb4f24291da83c4fa4d22c123723c1203
ineesgoncalvees <ines98goncalves@gmail.com>
2018-12-27 17:15:49 +0000

    Made GameLoop

--- SnakeGame/GameManager.cs
+++ SnakeGame/GameManager.cs
@@ -8,66 +8,48 @@ namespace SnakeGame
 {
     class GameManager
     {
+        private InterfaceManager im { get; }
 
-        public Draw draw;
-        public bool gameOver;
-        public bool reset;   
+        private Random rnd;
+        private ConsoleKeyInfo pressedKey;
+        
+        private bool gameOver;
+        private bool reset;
+
+        public GameManager()
+        {
+            rnd = new Random();
+            pressedKey = new ConsoleKeyInfo();
+        }
+
+        public void Start()
+        {
+
+            im.dir = "RIGHT";
+            im.pre_dir = "";
+            im.points = 0;
+            im.nTail = 0;
+
+            gameOver = false;
+            reset = false;
+            im.isPrinted = false;
+
+            im.noseX = InterfaceManager.width / 2;
+            im.noseY = InterfaceManager.height / 2;
+            im.appleX = rnd.Next(1, InterfaceManager.width - 1);
+            im.appleY = rnd.Next(1, InterfaceManager.height - 1);
+        }
 
         public void Update()
         {
+            InterfaceManager im = new InterfaceManager();
 
-        }
-
-        public int ReadEntry() {
-            // A loop that will run while the condition is true, and while the
-            // player doesn't choose to leave
-            while (true) {
-                // A variable needed for the cases
-                int entry;
-
-                // A switch case that will work based on the player's input
-                switch (Console.ReadLine()) {
-
-                    // If the player chooses 1
-                    case "1":
-                        // Will save the input, place it on id and then convert it
-                        entry = Convert.ToInt32(Console.ReadLine());
-                        draw.ShowWorld(entry);
-                        break;
-
-                    // If the player chooses 2
-                    case "2":
-                        Console.WriteLine("There are no highscores yet :<\n");
-                        Console.ReadKey();
-                        draw.ShowMenu(entry);
-                        break;
-
-                    // If the player chooses 3
-                    case "3":
-                        draw.ShowCredits(entry);
-                        break;
-
-                    case "4":
-                        // Will break the loop and exit the program
-                        Environment.Exit(0);
-                        break;
-
-                    // A default message in case the player chooses an
-                    // unavailable number
-                    default:
-                        // Will print an error message, and ask them to type a
-                        // number again
-                        Console.WriteLine("Not a valid number. Try again.");
-                        // Reads the input
-                        Console.ReadLine();
-                        // Will clear the console
-                        Console.Clear();
-                        // Calls for the ShowMenu() method
-                        ShowMenu();
-                        break;
-                }
+            do
+            {
+                Console.Clear();
+                im.ShowMenu();
             }
-        }
-
+            while (gameOver == false);            
+        }      
     }
 }
