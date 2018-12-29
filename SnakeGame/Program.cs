17faad4a0f28aea51d7c1d4a239404f2a2ef1177
ineesgoncalvees <ines98goncalves@gmail.com>
2018-12-29 16:02:22 +0000

    Fixed bug

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeGame
+﻿namespace SnakeGame
 {
     class Program
     {
         static void Main(string[] args)
         {
             GameManager gm = new GameManager();
-
+            
             gm.GameLoop();
             
             
