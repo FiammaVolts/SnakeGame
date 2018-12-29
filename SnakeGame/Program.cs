a13151187419b4ce64c4c5eea04ccc0221259cae
FiammaV <ines.perdigao.nunes@gmail.com>
2018-12-29 17:38:37 +0000

    Added comments

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace SnakeGame
+﻿using System;
+using System.Text;
+
+namespace SnakeGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Uses the UTF8 on the console
+            Console.OutputEncoding = Encoding.UTF8;
+            // Creates a new instance of GameManager
             GameManager gm = new GameManager();
             
+            // Calls GameLoop()
             gm.GameLoop();
             
             
